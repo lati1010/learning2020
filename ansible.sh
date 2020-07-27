dcab18d7846d3f17ded7c8b0550c7114dfde6f58
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2020-07-27 15:22:47 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
-apt update
-apt install software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 echo 'hi lavakumar'
